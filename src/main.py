344c9e2e3a529a5fd16afc75ed21af93987c5254
不见伊 <l15510265616@163.com>
2024-06-22 09:05:04 +0800

    update commit

--- src/main.py
+++ src/main.py
@@ -29,31 +29,43 @@ if mode == "1":
             file_tuple = os.path.split(file_path)
             result = rules.rules.delete(character, file_tuple)
             dst = os.path.join(result[0], result[1])
-            os.rename(src=file_path, dst=dst)
-            time.sleep(0.05)
+            try:
+                os.rename(src=file_path, dst=dst)
+                time.sleep(0.02)
+            finally:
+                pass
     elif rename_rule == "2":
         add_rule = input("""[1]在文件名后加序号
 [2]在文件名前加序号
-[3]在文件名后加等位序号
-[4]在文件名前加等位序号
-[5]在文件名后加自定义内容
-[6]在文件名前加自定义内容""")
-        if add_rule == "1":
-            pass
-        elif add_rule == "5" or add_rule == "6":
+[3]在文件名后加自定义内容
+[4]在文件名前加自定义内容
+""")
+        if add_rule == "1" or add_rule == "2":
+            for index, file_path in tqdm(enumerate(file_paths), desc='renaming:'):
+                file_tuple = os.path.split(file_path)
+                if add_rule == "1":
+                    result = rules.rules.addNumberBehind(
+                        number=index, file=file_tuple)
+                elif add_rule == "2":
+                    result = rules.rules.addNumberAhead(
+                        number=index, file=file_tuple)
+                dst = os.path.join(result[0], result[1])
+                os.rename(src=file_path, dst=dst)
+                time.sleep(0.02)
+        elif add_rule == "3" or add_rule == "4":
             character = input("请输入要添加的内容：")
             for file_path in tqdm(file_paths, desc='renaming:'):
-                if add_rule == "5":
+                if add_rule == "3":
                     file_tuple = os.path.split(file_path)
                     result = rules.rules.addBehind(
                         character=character, file=file_tuple)
-                elif add_rule == "6":
+                elif add_rule == "4":
                     file_tuple = os.path.split(file_path)
                     result = rules.rules.addAhead(
                         character=character, file=file_tuple)
                 dst = os.path.join(result[0], result[1])
                 os.rename(src=file_path, dst=dst)
-                time.sleep(0.05)
+                time.sleep(0.02)
 
 
 print("已完成重命名")
--- src/rules/rules.py
+++ src/rules/rules.py
@@ -6,12 +6,26 @@ class rules():
         result = (file[0], file[1].replace(character, ''))
         return result
 
-    # [5]
-    def addBehind(self, character, file):
-        result = (file[0], file[1]+character)
+    # [1]
+    def addNumberBehind(self, number, file):
+        filename = file[1].split('.')
+        filename = filename[0]+str(number)+'.'+filename[1]
+        result = (file[0], filename)
         return result
 
-    # [6]
+    # [2]
+    def addNumberAhead(self, number, file):
+        result = (file[0], str(number)+file[1])
+        return result
+
+    # [3]
+    def addBehind(self, character, file):
+        filename = file[1].split('.')
+        filename = filename[0]+character+'.'+filename[1]
+        result = (file[0], filename)
+        return result
+
+    # [4]
     def addAhead(self, character, file):
         result = (file[0], character+file[1])
         return result
